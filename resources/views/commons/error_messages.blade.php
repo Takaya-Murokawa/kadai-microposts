651d8e6d05987f56a6e7c4395f7e78e0d1a25156
Takaya-Murokawa <takaya-murokawa@dac.co.jp>
2020-06-08 02:27:51 +0000

    init

---
+++ resources/views/commons/error_messages.blade.php
@@ -0,0 +1,8 @@
+
+rs) > 0)
+    <ul class="alert alert-danger" role="alert">
+        @foreach ($errors->all() as $error)
+            <li class="ml-4">{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
